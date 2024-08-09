6a11aa46fd26ab2e9c5d10e1df7dffab452f672a
Chichari2 <100235931+Chichari2@users.noreply.github.com>
2024-08-09 09:46:48 +0200

    created json file, basic HTML-page

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
 
+#Load blog posts from JSON file
+def load_posts():
+    with open("posts.json", "r") as fileobj:
+        return json.load(fileobj)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()  # Fetch the blog posts from the JSON file
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
